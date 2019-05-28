0e6c767a8e2fc266da925e899293a05986e28dcf
Tra <trauvmd00621@fpt.edu.vn>
2019-05-28 18:14:25 +0700

    init map

--- app/src/main/java/com/example/dev20/MainActivity.java
+++ app/src/main/java/com/example/dev20/MainActivity.java
@@ -1,13 +1,55 @@
 package com.example.dev20;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Dialog;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GoogleApiAvailability;
 
 public class MainActivity extends AppCompatActivity {
+    private static final String TAG = "MainActivity";
+    private static final int ERROR_DIALOG_REQUEST = 9001;
 
-    @Override
+    EditText ed1,ed2,ed3;
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        if (isServicesOK()){
+            openMap();
+        }
+
+    }
+    private void openMap(){
+        Intent intent = new Intent(MainActivity.this, MapActivity.class);
+        startActivity(intent);
+    }
+    public boolean isServicesOK(){
+        Log.d(TAG, "isServicesOK: checking GG services version");
+        int available = GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MainActivity.this);
+
+        if (available == ConnectionResult.SUCCESS){
+            Log.d(TAG, "isServicesOK: GG Play Services is working");
+            return true;
+        }
+        else if (GoogleApiAvailability.getInstance().isUserResolvableError(available)){
+            Log.d(TAG, "isServicesOK: error but fixable");
+            Dialog dialog = GoogleApiAvailability.getInstance().getErrorDialog(MainActivity.this, available, ERROR_DIALOG_REQUEST);
+            dialog.show();
+        }
+        else {
+            Toast.makeText(this, "We can't make map requests", Toast.LENGTH_SHORT).show();
+        }
+        return false;
     }
 }
